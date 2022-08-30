838cb10503e5501d0d6762187c96f65a500233a2
Francsico Monti <montifranciscojavier@gmail.com>
2022-08-30 20:51:02 -0300

    se carga promise de productos y spinn de espera

--- src/containers/itemListContainer/itemListContainer.jsx
+++ src/containers/itemListContainer/itemListContainer.jsx
@@ -1,36 +1,53 @@
 import React, { useState, useEffect } from 'react'
 import ItemList from '../../components/itemList/itemList'
 
+
 const ItemListContainer = () => {
 
     const [listaDeProductos, setListaDeProductos] = useState([])
+    const [load, setLoad] = useState(false)
     
+    const promesa = new Promise((respoonse)=>{
+        setTimeout(async() => {
+            const dataResponse = await fetch('https://api.mercadolibre.com/sites/MLA/search?q=zapatilla_nike');
+            let data = await dataResponse.json();
+            setLoad(true);
+            respoonse(data);
+        },2000);
+    });
+
 
     useEffect(() => {
 
         const productos = async () => {
             try {
-                const response = await fetch('https://api.mercadolibre.com/sites/MLA/search?q=zapatilla_nike')
-                let data = await response.json();
+                const data = await promesa;
                 data.results.splice(0,1) //elimino primer objeto ya que se ve mal la foto y la descipcion
                 setListaDeProductos(data.results);
             }
             catch(e){
                 console.log(e);
-
-            }
-        
-    }
+            } 
+    };
         
         productos()    
-    }, []);
+    },);
 
-    return (
-        <>
-            <ItemList prod={listaDeProductos} />
-        </>
-    )
+    return <>{load ? <ItemList prod={listaDeProductos} /> : <div style={load_blq}><img src={ 'img/spinner.gif'} style={spinner_style} alt="Loading"></img>
+    <p>Estamos Cargando tu Proxima Compra...</p></div>} </>
 
 }
 
+
+const load_blq = {
+    display: 'flex',
+    flexDirection: "column",
+    margin: "300px auto",
+    width: "400px",
+    alignItems: "center",
+}
+const spinner_style = {
+    width: "200px",
+    objectFit: "cover",
+}
 export default ItemListContainer
